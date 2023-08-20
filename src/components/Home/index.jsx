76c4359ab5905bd97d12051971fee8a92641346c
IWhitebird <115157819+IWhitebird@users.noreply.github.com>
2023-08-20 23:20:36 +0530

    yo

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -5,11 +5,11 @@ import './Home.css'
 const Home = () => {
   return (
     <>
-    <div className='h-[100vh]'>
-      <div className='text-center text-5xl my-auto mt-[20rem]'>
+    <div className>
+      <div className='w-[1280px] text-center text-5xl mx-auto mt-[20rem]'>
         <DecoderText delay={0} className={'font-mons text-[#2f2f2f]'} text='SHREYAS PATANGE'/>
         
-        <div className="home__titletext mx-auto">
+        <div className="home__titletext">
             <p id="animtext">
               <span>Web Developer</span>
             </p>
